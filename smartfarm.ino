5cdc100f915605e5a3cf4227effe03b99dd819bc
seokyun_07 <79696739+seokyun0706@users.noreply.github.com>
2024-11-05 00:50:18 +0900

    Update smartfarm.ino

--- smartfarm.ino
+++ smartfarm.ino
@@ -1,9 +1,11 @@
 #include <LedControl.h>
 LedControl lc = LedControl(12, 11, 10, 1); //LedControl(DIN, CLK, CS, NUM); (NUM은 연결된 도트매트릭스의 개수)
 
+int aa = 6;
+int ab = 5;
 unsigned long delaytime = 100;
 
-byte happy[] = {. //매투락스값이 8개가 아님(8줄로 고침)
+byte happy[] = {
   B00000000,
   B01000010,
   B10100101,
@@ -14,7 +16,7 @@ byte happy[] = {. //매투락스값이 8개가 아님(8줄로 고침)
   B00000000
 };
 
-byte bad[] = {. //8줄로 고침
+byte bad[] = {
   B00000000,
   B10100101,
   B01000010,
@@ -27,8 +29,11 @@ byte bad[] = {. //8줄로 고침
 
 void setup() {
   lc.shutdown(0, false);  //절전모드를 꺼줌(원래는 절전모드임)  (0,false)에서 0은 도트매트릭스 번호 의미(하나만 사용하면 0번째임)
-  lc.setIntensity(0, 8); //밝기 설정
+  lc.setIntensity(0, 7); //밝기 설정
   lc.clearDisplay(0);  //화면 clear
+
+  pinMode(aa, OUTPUT);
+  pinMode(ab, OUTPUT);
 }
 
 void loop() {
@@ -38,9 +43,15 @@ void loop() {
   if (moisture > 600) {
     draw(bad);
     delay(delaytime);
+    digitalWrite(aa, HIGH);
+    digitalWrite(ab, LOW);
+    delay(1000);
   } else {
     draw(happy);
     delay(delaytime);
+    digitalWrite(aa, LOW);
+    digitalWrite(ab, LOW);
+    delay(1000);
   }
 }
 
@@ -49,6 +60,3 @@ void draw(byte arr[]) {
     lc.setRow(0, i, arr[i]);
   }
 }
-
-
-//이 코드로 실해햐보고 안되면 회로바꿔서 다시
